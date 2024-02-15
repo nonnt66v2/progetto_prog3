1c3091f153c9d549b885b8ed24965d2cd6e2f605
nonnt66v2 <91503962+nonnt66v2@users.noreply.github.com>
2024-02-15 12:54:49 +0100

    add doc

---
+++ progetto/biciclette/demo/src/main/java/singleton/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"singleton","l":"ConnessioneSingleton"},{"p":"singleton","l":"Database"}];updateSearchResults();
